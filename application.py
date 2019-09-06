d1101afd848348fe6e28c1c52ca787532806d90c
Casneil Simpson <46893725+Casneil@users.noreply.github.com>
2019-09-06 08:49:47 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from flask import Flask, render_template, url_for, flash,\
-    redirect, jsonify, request
+ redirect, jsonify, request
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 from forms import RegistrationForm, LoginForm
@@ -22,7 +22,7 @@ import httplib2
 from oauth2client import client
 import google.oauth2.credentials
 from authlib.client import OAuth2Session
-import functools
+from functools import wraps
 import os
 
 
@@ -110,14 +110,16 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps(
-            'Current user isalready connected.'), 200)
+        response = make_response(json.dumps('Current user is \
+          already connected.'),
+                                 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
+
 
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
@@ -129,6 +131,7 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
+
 
     output = ''
     output += '<h1>Welcome, '
@@ -144,14 +147,14 @@ def gconnect():
     return output
 
 
-# Creating a User #
+# Creating a User  Maybe need to delete #
 def newUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
-        'email'], picture=login_session['picture'])
+                   'email'], picture=login_session['picture'])
     session.add(newUser)
-    session.commit()
+    session.commit()       
     user = session.query(User).filter_by(email=login_session['email']).one()
-    return user.id
+    return user.id                       
 
 
 def getUserInfo(user_id):
@@ -164,28 +167,41 @@ def getUserID(email):
         user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
-        return None
+        return None   
 
-
+        
 # JSON APIs to view Shop Information
-@app.route('/shop/all/JSON')
+@app.route('/mall/all/JSON')
 def allShopsJSON():
     shops = session.query(Mall).all()
-    return jsonify(shops=[s.serialize for s in shops])
+    return jsonify(shops=[s.serialize for s in shops])   
 
 
-@app.route('/shop/items/all/JSON')
+@app.route('/mall/items/all/JSON')
 def allItemsJSON():
     items = session.query(Items).all()
-    return jsonify(items=[i.serialize for i in items])
+    return jsonify(items=[i.serialize for i in items])    
 
 
-@app.route('/shop/<int:mall_id>/item/JSON')
+@app.route('/mall/<int:mall_id>/item/JSON')
 def shopItemsJSON(mall_id):
     mall = session.query(Mall).filter_by(id=mall_id).one()
     items = session.query(Items).filter_by(
         mall_id=mall_id).all()
-    return jsonify(Items=[i.serialize for i in items])
+    return jsonify(Items=[i.serialize for i in items])    
+
+
+#Login Decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "username" in login_session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login to perform this action")
+            return redirect(url_fir('/glogin'))   
+    return wrap 
+
 
 
 # Displayes Shops #
@@ -195,8 +211,8 @@ def showShops():
     shops = session.query(Mall).order_by(asc(Mall.department))
     if 'username' not in login_session:
         return render_template('publicMall.html', shops=shops)
-    else:
-        return render_template('home.html', shops=shops)
+    else:    
+        return render_template('home.html', shops=shops)     
 
 
 # Displays Items within a shop #
@@ -204,106 +220,96 @@ def showShops():
 @app.route('/item/<int:mall_id>/items/')
 def showItems(mall_id):
     shops = session.query(Mall).filter_by(id=mall_id).all()
+    # creator = getUserInfo(mall.user_id)
     items = session.query(Items).filter_by(mall_id=mall_id).all()
-    return render_template('items.html', title='All Items',
-                           items=items, shops=shops,)
+    return render_template('items.html', title= 'All Items',\
+     items=items, shops=shops,)  
 
 
 # Show links to edit/delete or create a new Mall #
-@app.route('/home/new/', methods=['GET', 'POST'])
+@app.route('/home/new/', methods= ['GET','POST'])
+@login_required
 def newMall():
-    if 'username' not in login_session:
-        return redirect('/glogin')
+    # if 'username' not in login_session:
+    #     return redirect('/glogin')
     if request.method == 'POST':
         global newMall
-        newMall = Mall(department=request.form
-                       ['department'], description=request.form['description'],
-                       user_id=login_session['username'])
+        newMall = Mall(department=request.form\
+            ['department'], description=request.form['description'],\
+            user_id=login_session['username'])
         session.add(newMall)
         session.commit()
         flash('Shop %s Successfully Created' % newMall.department)
         return redirect(url_for('showShops'))
     else:
-        return render_template('newMall.html', newMall=newMall)
+       return render_template('newMall.html', newMall=newMall)    
 
 
 # Edit a Shop #
 @app.route('/home/<int:mall_id>/edit/', methods=['GET', 'POST'])
+@login_required
 def editedMall(mall_id):
-    if 'username' not in login_session:
-        return redirect('/glogin')
     editedMall = session.query(Mall).filter_by(id=mall_id).one()
     if editedMall.user_id != login_session['username']:
-        return "<script>function myFunction()\
-        {alert('Error!. Please create your own Shop in order to edit.')\
-        ;}</script><body onload='myFunction()'>"
+        return "<script>function myFunction() {alert('You are not authorized to edit this Shop. Please create your own Shop in order to edit.');}</script><body onload='myFunction()'>"        
     if request.method == 'POST':
         if request.form['department']:
             editedMall.department = request.form['department']
         if request.form['description']:
             editedMall.description = request.form['description']
         session.add(editedMall)
-        session.commit()
-        flash('Successfully Edited!: %s' % editedMall.department)
+        session.commit()    
+        flash('Successfully Edited!: %s' % editedMall.department)    
         return redirect(url_for('showShops'))
     else:
-        return render_template('editMall.html', mall_id=mall_id,
-                               mall=editedMall)
+        return render_template('editMall.html',mall_id=mall_id,\
+            mall=editedMall)
 
 
-@app.route('/home/<int:mall_id>/delete/', methods=['GET', 'POST'])
+@app.route('/home/<int:mall_id>/delete/', methods=['GET','POST']) 
+@login_required
 def deleteMall(mall_id):
-    if 'username' not in login_session:
-        return redirect('/glogin')
-    mallToDelete = session.query(Mall).filter_by(id=mall_id).one()
-    if mallToDelete.user_id != login_session['username']:
-        return "<script>function myFunction() {alert('Error!.\
-            Please create your own Shopin order to delete.');}\
-            </script><body onload='myFunction()'>"
-    if request.method == 'POST':
-        session.delete(mallToDelete)
-        session.commit()
-        return redirect(url_for('showItems', mall_id=mall_id))
-    else:
-        return render_template('deleteMall.html', mall_id=mall_id,
-                               mall=mallToDelete)
-
+       mallToDelete = session.query(Mall).filter_by(id=mall_id).one()
+       if mallToDelete.user_id != login_session['username']:
+           return "<script>function myFunction() {alert('You are not authorized to delete this Shop. Please create your own Shop in order to delete.');}</script><body onload='myFunction()'>" 
+       if request.method == 'POST':
+          session.delete(mallToDelete)
+          session.commit()
+          return redirect(url_for('showItems', mall_id=mall_id))
+       else:
+           return render_template('deleteMall.html', mall_id=mall_id,\
+            mall=mallToDelete)  
+            
 
 # Show links to create/edit/delete or create a new Item
-@app.route('/item/<int:mall_id>/item/new', methods=['GET', 'POST'])
-def newItem(mall_id):
-    if 'username' not in login_session:
-        return redirect('/glogin')
+@app.route('/item/<int:mall_id>/item/new', methods= ['GET','POST'])
+@login_required
+def newItem(mall_id): 
     newItem = session.query(Mall).filter_by(id=mall_id).one()
     if newItem.user_id != login_session['username']:
-        return "<script>function myFunction() {alert('Error!. Please\
-        create your own Shop in order to add\
-        items.');}</script><body onload='myFunction()'>"
+        return "<script>function myFunction() {alert('You are not authorized to add to this Shop. Please create your own Shop in order to add items.');}</script><body onload='myFunction()'>" 
     # global newItem
     if request.method == 'POST':
-        newItem = Items(name=request.form['name'], description=request.form
-                        ['description'], price=request.form['price'],
-                        mall_id=mall_id, user_id=newItem.user_id)
+        newItem = Items(name=request.form['name'], description=request.form\
+            ['description'], price=request.form['price'], mall_id=mall_id,\
+            user_id=newItem.user_id)
         session.add(newItem)
         session.commit()
-        flash('New Item %s Item added!' % (newItem.name))
+        flash('New Item %s Item added!' %(newItem.name))
         return redirect(url_for('showItems', mall_id=mall_id))
     else:
-        return render_template('createNewItem.html',
-                               newItem=newItem, mall_id=mall_id)
+        return render_template('createNewItem.html',\
+            newItem=newItem, mall_id=mall_id)    
 
 
-@app.route('/item/<int:mall_id>/item/<int:item_id>/edit',
-           methods=['GET', 'POST'])
+@app.route('/item/<int:mall_id>/item/<int:item_id>/edit', methods=['GET', 'POST'])
+@login_required
 def editedItem(mall_id, item_id):
-    if 'username' not in login_session:
-        return redirect('/glogin')
-    editedItem = session.query(Items).filter_by(id=item_id).one()
+    editedItem = session.query(Items).filter_by(id=item_id).one()    
     shops = session.query(Mall).filter_by(id=mall_id).all()
     if editedItem.user_id != login_session['username']:
-        return "<script>function myFunction() {alert('You are not authorized to\
-        edit this item. Please create your own Shop in order to edit items.\
-        ');}</script><body onload='myFunction()'>"
+        flash("Cannot edit items you did not created")
+        return redirect(url_for('showItems', mall_id=mall_id))
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
@@ -312,41 +318,37 @@ def editedItem(mall_id, item_id):
         if request.form['price']:
             editedItem.name = request.form['price']
         session.add(editedItem)
-        session.commit()
-        flash('Successfully Edited!: %s' % editedItem.name)
+        session.commit()    
+        flash('Successfully Edited!: %s' % editedItem.name)    
         return redirect(url_for('showItems', mall_id=mall_id))
     else:
-        return render_template('editItem.html', item=editedItem,
-                               item_id=item_id, mall_id=mall_id)
+        return render_template('editItem.html',item=editedItem,item_id=item_id,\
+            mall_id=mall_id)
 
 
-@app.route('/item/<int:mall_id>/item/<int:item_id>/delete',
-           methods=['GET', 'POST'])
+@app.route('/item/<int:mall_id>/item/<int:item_id>/delete', methods= ['GET','POST']) 
+@login_required
 def deleteItem(mall_id, item_id):
-    if 'username' not in login_session:
-        return redirect('/glogin')
-        editedItem = session.query(Items).filter_by(id=item_id).all()
+    editedItem = session.query(Items).filter_by(id=item_id).all()
     shops = session.query(Mall).filter_by(id=mall_id).all()
     itemToDelete = session.query(Items).filter_by(id=item_id).one()
     if itemToDelete.user_id != login_session['username']:
-        return "<script>function myFunction() {alert('You are not authorized to delete\
-        this item. Please create your own Shop in order to delete items.\
-        ');}</script><body onload='myFunction()'>"
+          flash("Cannot delete items you did not created")
+          return redirect(url_for('showItems', mall_id=mall_id))
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
         flash('Item %s Item Deleted!' % (itemToDelete.name))
         return redirect(url_for('showItems', mall_id=mall_id))
     else:
-        return render_template('deleteItem.html', title='Delete Item',
-                               itemToDelete=itemToDelete, mall_id=mall_id,
-                               item_id=item_id)
+        return render_template('deleteItem.html', title='Delete Item',\
+         itemToDelete=itemToDelete, mall_id=mall_id, item_id=item_id)    
 
 
 # About Page #
 @app.route('/about/')
 def about():
-    return render_template('about.html', title='About Page')
+    return render_template('about.html', title='About Page')    
 
 
 # Account Page #
@@ -355,12 +357,12 @@ def account():
     if 'username' in login_session:
         user = session.query(User).filter_by().all()
         picture = login_session['picture']
-        return render_template('account.html', title='Account', user=user,
-                               picture=picture)
+        return render_template('account.html', title='Account', user=user,\
+         picture=picture)
     else:
         flash('Youre not authorized to view this page. Please\
             login to do so')
-        return redirect(url_for('showLogin'))
+        return redirect(url_for('showLogin'))    
 
 
 # Logout #
@@ -384,6 +386,7 @@ def logout():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
+
         response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -392,9 +395,9 @@ def logout():
         response = make_response(
             json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
-        return response
+        return response       
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)    
